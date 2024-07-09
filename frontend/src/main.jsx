11f7676102dd06110041b87fbd8edef7a66a046a
shivamshaw07 <shivamshaw9005@gmail.com>
2024-07-09 20:00:42 +0530

    Added analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,21 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from './reducer/index.js'
+import { Analytics } from '@vercel/analytics/react'
 
 const store = configureStore({
   reducer: rootReducer
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
   <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
   </Provider>
+  <Analytics/>
+  </>
  
 )
 
